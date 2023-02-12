5ff20dde14dcef5b4bfac6accd28c530e47fd268
Maxime Stefani <max341@free.fr>
2023-02-12 17:37:56 +0100

    Fix CORS issue by openning it to everyone

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   /*app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );*/
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
-  
   await app.listen(3333);
 }
 bootstrap();
